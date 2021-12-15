1daaa36be24a31bdf26431efdbcc2b2af8fe93f7
AndyMc7993 <87806626+AndyMc7993@users.noreply.github.com>
2021-12-15 19:02:35 +0000

    Deploy, links & name

--- src/components/Pfdata.js
+++ src/components/Pfdata.js
@@ -9,20 +9,20 @@ export default [
         title: "bearbnb",
         description: "A simple grizzly airbnb clone with basic prop inheritance.",
         coverImg: bearbnb,
-        link: "www.yes.com",
+        link: "https://andymc7993.github.io/bearbnb/",
     },
     {
         id: 2,
         title: "Weather vs. Glasgow",
         description: "API integrated weather app showcasing the radiant gloominess of my home city.",
-        coverImg: percussion,
-        link:"www.yes.com",
+        coverImg: weather,
+        link:"https://andymc7993.github.io/weather-comparison/",
     },
     {
         id: 3,
         title: "Percussion",
         description: "Orchestral beats styled react app.",
-        coverImg: weather,
-        link:"www.yes.com",
+        coverImg: percussion,
+        link:"https://andymc7993.github.io/percussion-app/",
     }
 ]
