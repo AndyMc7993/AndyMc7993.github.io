0367266ddaf489666302553d1563d02bdd5eb8f5
AndyMc7993 <87806626+AndyMc7993@users.noreply.github.com>
2021-12-15 17:56:50 +0000

    Deploy w/ correct links

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,7 +8,7 @@ export default function Contact() {
                 <h3 className="footer-h3"> Want to learn <br /><span id="footer-more">more</span> ? </h3>
                 <div className="footer-links">
                     <a href="mailto:andymcinnes7993@gmail.com">email me</a>
-                    <a href="https://../images/Andy McInnes - CV 2021.pdf">my CV</a>
+                    <a href='https://drive.google.com/file/d/1c5nZsxISkOtKOR-bpczaIvGPJklj9M_k/view?usp=sharing'>my CV</a>
                     <a href="https://github.com/AndyMc7993">my github</a>
                     <a href="https://linkedin.com/in/andy-mcinnes-06b54919b">my linkedin</a>
                 </div>
